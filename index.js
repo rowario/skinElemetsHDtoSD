5f6a04ee00b29927a82150c06e6d208453ac4d62
r0wario <kumanist@gmail.com>
2019-06-25 05:34:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,24 @@ fs.readdir(resizeFolder, (err, files) => {
 	});
 });
 
-console.log("Files to change:");
-filesArr.forEach(file => {
-	console.log(file);
-});
-filesArr.forEach(file => {
-	sizeOf(resizeFolder + file, function (err, dimensions) {
-		var newWidth = parseInt(dimensions.width / 2);
-		var newHeight = parseInt(dimensions.height / 2);
-		var newFilename = resizeFolderOut + file.replace("@2x","");
-		sharp(resizeFolder + file).resize({ width: newWidth,height: newHeight }).toFile(newFilename)
-			.then(function(newFileInfo) {
-				console.log(newFilename + " Success")
-			})
-			.catch(function(err) {
-				console.log(newFilename + " Error occured");
-			});
+setTimeout(function () {
+	console.log("Files to change:");
+	filesArr.forEach(file => {
+		console.log(file);
 	});
-});
+	filesArr.forEach(file => {
+		sizeOf(resizeFolder + file, function (err, dimensions) {
+			var newWidth = parseInt(dimensions.width / 2);
+			var newHeight = parseInt(dimensions.height / 2);
+			var newFilename = resizeFolderOut + file.replace("@2x","");
+			sharp(resizeFolder + file).resize({ width: newWidth,height: newHeight }).toFile(newFilename)
+				.then(function(newFileInfo) {
+					console.log(newFilename + " Success")
+				})
+				.catch(function(err) {
+					console.log(newFilename + " Error occured");
+				});
+		});
+
+	});
+},1000);
