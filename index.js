d3c81ecb22dcc57af2e141625e9847afb7713111
r0wario <kumanist@gmail.com>
2019-06-25 05:24:19 +0300

    s
    send script

---
+++ index.js
@@ -0,0 +1,35 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const sizeOf = require('image-size');
+
+let resizeFolder = './to_resize/';
+let resizeFolderOut = './output/';
+let filesArr = [];
+
+fs.readdir(resizeFolder, (err, files) => {
+	files.forEach(file => {
+		filesArr.push(file);
+	});
+});
+
+setTimeout(function () {
+	console.log("Files to change:");
+	filesArr.forEach(file => {
+		console.log(file);
+	});
+	filesArr.forEach(file => {
+		sizeOf(resizeFolder + file, function (err, dimensions) {
+			var newWidth = parseInt(dimensions.width / 2);
+			var newHeight = parseInt(dimensions.height / 2);
+			var newFilename = resizeFolderOut + file.replace("@2x","");
+			sharp(resizeFolder + file).resize({ width: newWidth,height: newHeight }).toFile(newFilename)
+				.then(function(newFileInfo) {
+					console.log(newFilename + " Success")
+				})
+				.catch(function(err) {
+					console.log(newFilename + " Error occured");
+				});
+		});
+
+	});
+},1000);
